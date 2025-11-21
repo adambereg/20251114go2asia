3c35e8c122ead422ffdfabeda0fafcac954afa49
adambereg <fred89059599296@gmail.com>
2025-11-21 11:41:19 +0700

    fix: remove unused useUser import from page.tsx

--- apps/go2asia-pwa-shell/app/page.tsx
+++ apps/go2asia-pwa-shell/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useUser } from '@clerk/nextjs';
 import { useAuthMode } from '../contexts/AuthModeContext';
 import {
   MapPin,
